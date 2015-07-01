fc7476853bab7d9222af702652970f72e17d1ad5
Dogan Yazar <dogan.yazar@scypho.com>
2015-07-01 15:23:19 +0000

    Restructure bluez defs

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 'use strict';
 var service = require('./lib/service.js');
 var manager = require('./lib/bluez-mgr.js');
+var Defs = require('./lib/bluez-defs.js');
 
 module.exports = {
     Service: service.Service,
     Characteristic: service.Characteristic,
     Descriptor: service.Descriptor,
-    bluezManager: new manager.BluezManager()
+    bluezManager: new manager.BluezManager(),
+    Defs: Defs
 };
