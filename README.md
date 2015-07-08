0f09a1cc5c0016bb373fc69b03ef3f640a0fe7a6
Dogan Yazar <dogan.yazar@scypho.com>
2015-07-08 15:12:32 +0000

    Add an example

--- README.md
+++ README.md
@@ -3,3 +3,7 @@ A node.js wrapper for Bluez Dbus API for peripherals
 
 ## Prerequisites
 Bluez 5.0
+
+
+## Usage
+Check the examples directory for usage. 
---
+++ examples/hello.js
@@ -0,0 +1,51 @@
+'use strict';
+var nybluez = require('../');
+var Service = nybluez.Service;
+var Characteristic = nybluez.Characteristic;
+var Descriptor = nybluez.Descriptor;
+var bluezManager = nybluez.bluezManager;
+var Defs = nybluez.Defs;
+
+var helloCharUserDescriptorImpl = {
+    ReadValue: function() {
+        return new Buffer('Wifi');
+    }
+};
+
+var helloCharImpl = {
+    localValue: 'NOT_INIT',
+
+    ReadValue: function() {
+        return new Buffer(this.localValue);
+    },
+
+    WriteValue: function(value) {
+        this.localValue = value.toString();
+    }
+};
+
+var helloCharUserDescriptor = new Descriptor(
+                                'helloCharUserDescriptor',
+                                Defs.UUIDs.CharacteristicUserDescription,
+                                ['read'],
+                                helloCharUserDescriptorImpl);
+
+var helloChar = new Characteristic(
+                'helloChar',
+                'dddd5678-1234-5678-1234-56789dddddd1',
+                ['read', 'write', 'notify'],
+                [helloCharUserDescriptor],
+                helloCharImpl);
+
+var helloService = new Service(
+                    'confService',
+                    '88888888-1111-2222-3333-56789dddddd0',
+                    true,
+                    [helloChar]);
+
+bluezManager.init(function(err) {
+    if (err) {
+        throw err;
+    }
+    bluezManager.registerBleServices([helloService]);
+});
